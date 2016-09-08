c74d71c4f229ee57b3744fc575ea33478dd96682
liz3 <accounts@liz3.de>
2016-09-08 17:59:58 +0200

    I added the missing debugger files!

---
+++ src/main/java/com/skriptide/guis/info/InfoGuiController.java
@@ -0,0 +1,23 @@
+package com.skriptide.guis.info;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * Created by Liz3ga on 05.09.2016.
+ */
+public class InfoGuiController {
+
+	@FXML
+	public Button okBtn;
+
+	@FXML
+	public Button cancelBtn;
+
+	@FXML
+	public Label label;
+
+
+
+}
